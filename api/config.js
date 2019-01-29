3ef74f846a7059ede73f2535293be35eda8c0e0b
krmirandas <kevinmiranda29@ciencias.unam.mx>
2019-01-29 00:45:07 -0600

    Formularios

--- api/config.js
+++ api/config.js
@@ -1,9 +1,18 @@
-var config = {
-    
-  facebook: {
-    id: '2245926022359801',
-    secret: '4b74fba7a1ab14626fa4949f4ee00e4d'
-  }
-};
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../models/User');
 
-module.exports = config;
+passport.use(new FacebookStrategy({
+    clientID: '2245926022359801',
+    clientSecret: '4b74fba7a1ab14626fa4949f4ee00e4d',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({userid: profile.id}, {name: profile.displayName,userid: profile.id}, function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+module.exports = passport;
--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { PublishComponent } from './publish/publish.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    PublishComponent
+   
   ],
   imports: [
     BrowserModule,
